bdd157d257a2b0db4ffe14f6e735b8b6dff39418
RollsRoycewk <50765560@qq.com>
2022-02-06 23:57:47 +0800

    feat: 多图上传/删除功能

--- pages/add-input/add-input.vue
+++ pages/add-input/add-input.vue
@@ -8,46 +8,56 @@
 		</uni-nav-bar>
 		<!-- 文本域 -->
 		<textarea v-model="content" class="uni-textarea px-2" placeholder="说一句话呗.........."></textarea>
+		<!-- 多图上传 -->
+		<upload-image @change="changeImage"></upload-image>
 		<!-- 底部操作条 -->
 		<view class="fixed-bottom bg-white flex align-center" style="height: 85rpx;">
 			<view class="iconfont icon-caidan footer-btn animated" hover-class="jello"></view>
 			<view class="iconfont icon-huati footer-btn animated" hover-class="jello"></view>
 			<view class="iconfont icon-tupian footer-btn animated" hover-class="jello"></view>
-			<view class="bg-main text-white ml-auto flex justify-center align-center rounded mr-2 animated" style="width: 140rpx;height: 60rpx;">发送</view>
+			<view class="bg-main text-white ml-auto flex justify-center align-center rounded mr-2 animated"
+				style="width: 140rpx;height: 60rpx;">发送</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue"
-	
+	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue";
+	import uploadImage from "@/components/common/upload-image.vue"
+
 	export default {
-		components:{
+		components: {
 			uniNavBar,
-			
+			uploadImage
 		},
 		data() {
 			return {
-				content:""
+				content: "",
+				imageList:[]
 			}
 		},
 		methods: {
 			// 返回上一步
-			goBack(){
+			goBack() {
 				uni.navigateBack();
+			},
+			// 选中图片
+			changeImage(e) {
+				console.log("e",e);
+				this.imageList = e;
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-.footer-btn{
-	width: 86rpx;
-	height: 86rpx;
-	display: flex;
-	justify-content: center;
-	align-content: center;
-	align-items: center;
-	font-size: 50rpx;
-}
+	.footer-btn {
+		width: 86rpx;
+		height: 86rpx;
+		display: flex;
+		justify-content: center;
+		align-content: center;
+		align-items: center;
+		font-size: 50rpx;
+	}
 </style>
