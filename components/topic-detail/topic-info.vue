0c1741291f327d78fd034c90fc9c489d94854470
RollsRoycewk <50765560@qq.com>
2022-02-13 23:50:08 +0800

    feat: [动态]话题详情tab选项卡切换

--- components/topic-detail/topic-info.vue
+++ components/topic-detail/topic-info.vue
@@ -4,27 +4,27 @@
 			<image :src="info.cover" mode="aspectFill" style="height: 300rpx;" class="w-100 filter">
 			</image>
 		</view>
-	
+
 		<view class="position-relative bg-white px-2 pb-2" style="z-index: 10;">
 			<view class="flex">
 				<image src="/static/demo/topicpic/1.jpeg" style="width: 150rpx;height: 150rpx;margin-top: -75rpx;"
 					class="rounded mr-2"></image>
 				<text class="font-md">#{{info.title}}#</text>
 			</view>
-	
+
 			<view class="flex align-center font text-secondary mt-1">
 				<text class="mr-2">动态:{{info.news_count}}</text>
 				<text>今日:{{info.today_count}}</text>
 			</view>
 			<view class="text-secondary font-md">{{info.desc}}</view>
 		</view>
-	
+
 	</view>
 </template>
 
 <script>
 	export default {
-		props:['info'],
+		props: ['info'],
 		data() {
 			return {
 
@@ -34,4 +34,11 @@
 </script>
 
 <style lang="scss" scoped>
+	.filter {
+		/*  
+	   filter(滤镜) 属性
+	   blur 给图像设置高斯模糊。"radius"一值设定高斯函数的标准差，或者是屏幕上以多少像素融在一起， 所以值越大越模糊；
+	  */
+		filter: blur(10px);
+	}
 </style>
--- pages/topic-detail/topic-detail.vue
+++ pages/topic-detail/topic-detail.vue
@@ -2,15 +2,83 @@
 	<view>
 		<topic-info :info="info"></topic-info>
 		<divider></divider>
+
+		<view class="p-2 flex align-center border-bottom" hover-class="bg-light
+		" v-for="(item,index) in hotList" :key="index">
+			<text class="iconfont icon-xihuan text-main"></text>
+			<text class="font text-dark text-ellipsis">{{item.title}}</text>
+		</view>
+		<divider></divider>
+
+		<!-- tab -->
+		<view class="flex align-center py-2">
+			<view class="flex align-center flex-1 justify-center" v-for="(item,index) in tabBars" :key="index"
+				:class="index === tabIndex ? 'font-lg font-weight-bold text-main' : 'font-md'"
+				@click="changeTab(index)">{{item.name}}</view>
+		</view>
+		
+		<!-- 列表 -->
+		<block v-for="(item,index) in list1" :key="index">
+			<common-list :item="item" :index="index"></common-list>
+		</block>
 	</view>
 </template>
 
 <script>
+	const demo = [{
+			username: "昵称",
+			userpic: "/static/default.jpg",
+			newstime: "2019-10-20 下午04:30",
+			isFollow: false,
+			title: "我是标题",
+			titlepic: "/static/demo/datapic/11.jpg",
+			support: {
+				type: "support", // 顶
+				support_count: 1,
+				unsupport_count: 2
+			},
+			comment_count: 2,
+			share_num: 2
+		},
+		{
+			username: "昵称",
+			userpic: "/static/default.jpg",
+			newstime: "2019-10-20 下午04:30",
+			isFollow: false,
+			title: "我是标题",
+			titlepic: "",
+			support: {
+				type: "unsupport", // 踩
+				support_count: 1,
+				unsupport_count: 2
+			},
+			comment_count: 2,
+			share_num: 2
+		},
+		{
+			username: "昵称",
+			userpic: "/static/default.jpg",
+			newstime: "2019-10-20 下午04:30",
+			isFollow: false,
+			title: "我是标题",
+			titlepic: "",
+			support: {
+				type: "", // 未操作
+				support_count: 1,
+				unsupport_count: 2
+			},
+			comment_count: 2,
+			share_num: 2
+		}
+	];
+
 	import topicInfo from "@/components/topic-detail/topic-info.vue";
-	
+	import commonList from "@/components/common/common-list.vue"
+
 	export default {
-		components:{
-			topicInfo
+		components: {
+			topicInfo,
+			commonList
 		},
 		data() {
 			return {
@@ -20,7 +88,19 @@
 					desc: "话题描述",
 					today_count: 0,
 					news_count: 10
-				}
+				},
+				hotList: [{
+					title: "【新人必读】uni-app实战第二季商城类项目开发"
+				}, {
+					title: "【新人必读】uni-app实战第三季仿微信项目开发"
+				}],
+				tabIndex: 0,
+				tabBars: [{
+					name: "默认"
+				}, {
+					name: "最新"
+				}],
+				list1:[]
 			}
 		},
 		onLoad(e) {
@@ -28,19 +108,17 @@
 				let res = JSON.parse(e.detail)
 				console.log(res);
 			}
+			this.list1 = demo
 		},
 		methods: {
-
+			// tab切换
+			changeTab(index) {
+				this.tabIndex = index
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.filter {
-		/*  
-	   filter(滤镜) 属性
-	   blur 给图像设置高斯模糊。"radius"一值设定高斯函数的标准差，或者是屏幕上以多少像素融在一起， 所以值越大越模糊；
-	  */
-		filter: blur(10px);
-	}
+
 </style>
