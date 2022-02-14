eb4b49f75c213fca75dfb13540f028763861ec2b
RollsRoycewk <50765560@qq.com>
2022-02-14 23:12:38 +0800

    feat: [消息列表]过滤器过滤时间

--- components/msg/msg-list.vue
+++ components/msg/msg-list.vue
@@ -5,7 +5,7 @@
 		<view class="flex flex-column flex-1">
 			<view class="flex align-center justify-between">
 				<text class="font-md">{{item.username}}</text>
-				<text class="text-secondary font">	{{item.update_time}}</text>
+				<text class="text-secondary font">	{{item.update_time | formatTime}}</text>
 			</view>
 			<view class="flex align-center justify-between">
 				<text class="text-secondary text-ellipsis"
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+	import $T from "@/common/time.js"
 	import uniBadge from "@/components/uni-ui/uni-badge/uni-badge.vue";
 
 	export default {
@@ -26,6 +27,13 @@
 		},
 		components: {
 			uniBadge
+		},
+		// 过滤器
+		filters:{
+			formatTime(value){
+				console.log("value",value);
+				return $T.gettime(value);
+			}
 		}
 	}
 </script>
