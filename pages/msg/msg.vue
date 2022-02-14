3863e7699c64f9f304b718a912d81410f700f0cf
RollsRoycewk <50765560@qq.com>
2022-02-14 23:03:19 +0800

    feat: 封装消息列表组件

--- pages/msg/msg.vue
+++ pages/msg/msg.vue
@@ -1,14 +1,46 @@
 <template>
 	<view>
-		消息页面
+		<!-- 消息列表 -->
+		<block v-for="(item,index) in list" :key="index">
+			<msg-list :item="item" :index="index"></msg-list>
+		</block>
 	</view>
 </template>
 
 <script>
+	import msgList from "@/components/msg/msg-list.vue";
+
 	export default {
+		components: {
+			msgList
+		},
 		data() {
 			return {
-				
+				list: [{
+					avatar: "/static/default.jpg",
+					username: "聊天信息",
+					update_time: 1570718427,
+					data: "聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊",
+					noread: 20
+				}, {
+					avatar: "/static/default.jpg",
+					username: "聊天信息",
+					update_time: 1570718427,
+					data: "聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊",
+					noread: 20
+				}, {
+					avatar: "/static/default.jpg",
+					username: "聊天信息",
+					update_time: 1570718427,
+					data: "聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊",
+					noread: 20
+				}, {
+					avatar: "/static/default.jpg",
+					username: "聊天信息",
+					update_time: 1570718427,
+					data: "聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊聊天信息啊啊",
+					noread: 20
+				}]
 			};
 		}
 	}
