a2ca277349d7ac5695ccbb08051f1bd30c153e89
RollsRoycewkk <RollsRoycewk@163.com>
2022-02-15 16:05:03 +0800

    feat: 下拉弹出框

--- pages/msg/msg.vue
+++ pages/msg/msg.vue
@@ -9,6 +9,18 @@
 		<template v-else>
 			<noThing></noThing>
 		</template>
+
+		<!-- 弹出层 -->
+		<uniPopup ref="popup" type="top">
+			<view class="flex align-center justify-center font-md border-bottom py-2" hover-class="bg-light"
+				@click="popupEvent('friend')">
+				<text class="iconfont icon-sousuo mr-2"></text> 添加好友
+			</view>
+			<view class="flex align-center justify-center font-md py-2" hover-class="bg-light"
+				@click="popupEvent('clear')">
+				<text class="iconfont icon-shanchu mr-2"></text> 清除列表
+			</view>
+		</uniPopup>
 	</view>
 </template>
 
@@ -41,12 +53,14 @@
 
 
 	import msgList from "@/components/msg/msg-list.vue";
-	import noThing from "@/components/common/no-thing.vue"
+	import noThing from "@/components/common/no-thing.vue";
+	import uniPopup from "@/components/uni-ui/uni-popup/uni-popup.vue";
 
 	export default {
 		components: {
 			msgList,
-			noThing
+			noThing,
+			uniPopup
 		},
 		data() {
 			return {
@@ -61,6 +75,16 @@
 		onPullDownRefresh() {
 			this.refresh()
 		},
+		// 监听原生标题栏按钮点击事件
+		onNavigationBarButtonTap(e) {
+			switch (e.index) {
+				case 0: // 左边
+					break;
+				case 1: // 右边
+					this.$refs.popup.open("top")
+					break;
+			}
+		},
 		methods: {
 			// 下拉刷新
 			refresh() {
@@ -68,6 +92,19 @@
 					this.list = demo;
 					uni.stopPullDownRefresh();
 				}, 2000)
+			},
+			// 弹出层选项点击事件
+			popupEvent(e) {
+				switch (e) {
+					case 'friend':
+						console.log('添加好友');
+						break;
+					case 'clear':
+						console.log('清除列表');
+						break;
+				}
+				// 关闭弹出层
+				this.$refs.popup.close()
 			}
 		}
 	}
