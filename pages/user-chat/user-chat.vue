cbe5f5f186f6354084d5206d107c3ada2a8fd46c
RollsRoycewk <50765560@qq.com>
2022-02-19 00:25:58 +0800

    feat: [消息]聊天页面开发

--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,19 +1,275 @@
 <template>
 	<view>
+		<scroll-view scroll-y style="position: absolute;left: 0;top: 0;right: 0;bottom: 100rpx;"
+			:scroll-into-view="scrollInto" scroll-with-animation>
+			<block v-for="(item,index) in list" :key="index">
+				<view :id="'chat'+index">
+					<user-chat :item="item" :index="index" :pretime="index > 0 ? list[index-1].create_time : 0">
+					</user-chat>
+				</view>
+			</block>
+
+		</scroll-view>
 		<!-- 底部操作条 -->
 		<view class="fixed-bottom border-top bg-white flex align-center" style="height: 100rpx;">
-			<input class="flex-1 rounded bg-light ml-2" type="text" value="" placeholder="文明发言" style="padding: 5rpx;"/>
-			<view class="iconfont icon-fabu flex align-center justify-center font-lg animated" style="width: 100rpx;" hover-class="jello text-main"></view>
+			<input class="flex-1 rounded bg-light ml-2" type="text" v-model="content" placeholder="文明发言"
+				style="padding: 5rpx;" @confirm="submit"/>
+			<view class="iconfont icon-fabu flex align-center justify-center font-lg animated" style="width: 100rpx;"
+				hover-class="jello text-main" @click="submit"></view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import userChat from "@/components/user-chat/user-chat.vue"
+
 	export default {
+		components: {
+			userChat
+		},
 		data() {
 			return {
-				
+				scrollInto: '',
+				content: "",
+				list: [{
+					user_id: 2,
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊001",
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: 1,
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊002",
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: 2,
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊003",
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, {
+					user_id: Date.now(),
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					data: "你好啊" + Date.now(),
+					type: "text",
+					create_time: 1570783530
+				}, ]
 			};
+		},
+		methods: {
+			// 发送消息
+			submit() {
+				if (!this.content) {
+					return uni.showToast({
+						title: "发送消息不能为空",
+						icon: "none"
+					})
+				}
+
+				let obj = {
+					user_id: 1,
+					avatar: "/static/default.jpg",
+					username: "昵称",
+					type: "text",
+					data: this.content,
+					create_time: (new Date).getTime()
+				}
+
+				this.list.push(obj);
+				// 清空输入框
+				this.content = '';
+				// 滚动到底部
+				this.pageToBottom();
+			},
+			// 滚动到底部
+			pageToBottom() {
+				let lastIndex = this.list.length - 1;
+				if (lastIndex < 0) return;
+				console.log("chat" + lastIndex);
+				this.scrollInto = "chat" + lastIndex;
+				console.log(this.scrollInto,"000000000000000000");
+			}
 		}
 	}
 </script>
