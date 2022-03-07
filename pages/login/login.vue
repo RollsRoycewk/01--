f8ecbfcc1d2d6c597f990773e76de8e03b6297d8
RollsRoycewkk <RollsRoycewk@163.com>
2022-03-07 17:14:20 +0800

    feat: 登录基本验证/获取验证码

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -2,64 +2,77 @@
 	<view>
 		<uni-status-bar></uni-status-bar>
 		<view>
-			<view class="iconfont icon-guanbi flex align-center justify-center font-lg" style="width: 100rpx;height: 100rpx;" hover-class="bg-light" @click="back"></view>
+			<view class="iconfont icon-guanbi flex align-center justify-center font-lg"
+				style="width: 100rpx;height: 100rpx;" hover-class="bg-light" @click="back"></view>
 		</view>
-		
-		<view class="text-center" style="padding-top: 130rpx;padding-bottom: 70rpx;font-size: 55rpx;">{{status ? '手机验证码登录' : '账号密码登录'}}</view>
-		
+
+		<view class="text-center" style="padding-top: 130rpx;padding-bottom: 70rpx;font-size: 55rpx;">
+			{{status ? '手机验证码登录' : '账号密码登录'}}
+		</view>
+
 		<view class="px-2">
 			<template v-if="!status">
 				<view class="mb-2">
-					<input type="text" placeholder="昵称/手机号/邮箱" class="border-bottom p-2"/>
+					<input type="text" placeholder="昵称/手机号/邮箱" class="border-bottom p-2" v-model="username" />
 				</view>
 				<view class="mb-2 flex align-stretch">
-					<input type="text" placeholder="请输入密码" class="border-bottom p-2 flex-1"/>
-					<view class="border-bottom flex align-center justify-center font text-muted" style="width: 150rpx;">忘记密码?</view>
+					<input type="text" placeholder="请输入密码" class="border-bottom p-2 flex-1" v-model="password" />
+					<view class="border-bottom flex align-center justify-center font text-muted" style="width: 150rpx;">
+						忘记密码?</view>
 				</view>
 			</template>
-			
+
 			<template v-else>
 				<view class="mb-2 flex align-stretch">
 					<view class="border-bottom flex align-center justify-center font px-2">+86</view>
-					<input type="text" placeholder="手机号" class="border-bottom p-2 flex-1"/>
+					<input type="text" placeholder="手机号" class="border-bottom p-2 flex-1" v-model="phone" />
 				</view>
 				<view class="mb-2 flex align-stretch">
-					<input type="text" placeholder="请输入验证码" class="border-bottom p-2 flex-2"/>
-					<view class="border-bottom flex align-center justify-center font-sm text-white bg-main" style="width: 180rpx;">获取验证码</view>
+					<input type="text" placeholder="请输入验证码" class="border-bottom p-2 flex-2" v-model="code" />
+					<view class="border-bottom flex align-center justify-center font-sm text-white bg-main"
+						:class="codeTime > 0 ? 'bg-main-disabled' : 'bg-main'" style="width: 180rpx;" @click="getCode">
+						{{codeTime > 0 ? codeTime + 's' : "获取验证码"}}</view>
 				</view>
 			</template>
-			
+
 
 		</view>
-		
+
 		<view class="py-2 px-3">
-			<button class="bg-main text-white" style="border-radius: 50rpx;border: 0;" type="primary">登录</button>
+			<button class="bg-main text-white" style="border-radius: 50rpx;border: 0;" type="primary"
+				:disabled="disabled" @click="submit">登录</button>
 		</view>
-		
+
 		<view class="flex align-center justify-center pt-2 pb-4">
-			<view class="text-primary font-sm"  @click="changeStatus">{{status?'账号密码登录':'验证码登陆'}}</view>
+			<view class="text-primary font-sm" @click="changeStatus">{{status?'账号密码登录':'验证码登陆'}}</view>
 			<view class="text-muted mx-2">|</view>
 			<view class="text-primary font-sm">登录遇到问题</view>
 		</view>
-		
+
 		<view class="flex align-center justify-center">
 			<view style="height: 1rpx;background-color: #dddddd;width: 100rpx;"></view>
 			<view class="mx-2 text-muted">社交账号登录</view>
 			<view style="height: 1rpx;background-color: #dddddd;width: 100rpx;"></view>
 		</view>
-		
+
 		<view class="flex align-center px-5 py-3">
 			<view class="flex align-center justify-center flex-1">
-				<view class="iconfont icon-QQ font-lg text-white bg-primary flex align-center justify-center rounded-circle" style="width: 100rpx;height: 100rpx;"></view>
+				<view
+					class="iconfont icon-QQ font-lg text-white bg-primary flex align-center justify-center rounded-circle"
+					style="width: 100rpx;height: 100rpx;"></view>
 			</view>
 			<view class="flex align-center justify-center flex-1">
-				<view class="iconfont icon-QQ font-lg text-white bg-primary flex align-center justify-center rounded-circle" style="width: 100rpx;height: 100rpx;"></view>
+				<view
+					class="iconfont icon-QQ font-lg text-white bg-primary flex align-center justify-center rounded-circle"
+					style="width: 100rpx;height: 100rpx;"></view>
 			</view>
 			<view class="flex align-center justify-center flex-1">
-				<view class="iconfont icon-QQ font-lg text-white bg-primary flex align-center justify-center rounded-circle" style="width: 100rpx;height: 100rpx;"></view>
+				<view
+					class="iconfont icon-QQ font-lg text-white bg-primary flex align-center justify-center rounded-circle"
+					style="width: 100rpx;height: 100rpx;"></view>
 			</view>
 		</view>
-		
+
 		<view class="flex align-center justify-center text-muted">
 			注册即代表同意<text class="text-primary">《xxx社区协议》</text>
 		</view>
@@ -68,23 +81,82 @@
 
 <script>
 	import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue';
-	
+
 	export default {
-		components:{
+		components: {
 			uniStatusBar
 		},
 		data() {
 			return {
-				status:false
+				status: false,
+				username: "",
+				password: "",
+				phone: "",
+				code: "",
+				codeTime: 0
 			};
 		},
-		methods:{
-			back(){
-				uni.navigateBack({delta:1})
+		computed: {
+			disabled() {
+				if ((this.username == '' || this.password == '') && (this.phone == '' || this.code == '')) {
+					return true
+				}
+				return false
+			}
+		},
+		methods: {
+			back() {
+				uni.navigateBack({
+					delta: 1
+				})
+			},
+			// 初始化表单
+			initForm() {
+				this.username = "";
+				this.password = "";
+				this.phone = "";
+				this.code = "";
 			},
 			// 切换登录方式
-			changeStatus(){
+			changeStatus() {
+				// 初始化表单
+				this.initForm();
 				this.status = !this.status;
+			},
+			// 获取验证码
+			getCode() {
+				if (this.codeTime) return;
+				// 倒计时
+				this.codeTime = 60;
+				let timer = setInterval(() => {
+					if (this.codeTime >= 1) {
+						this.codeTime--;
+					} else {
+						this.codeTime = 0;
+						clearInterval(timer)
+					}
+				}, 1000)
+			},
+			// 表单验证
+			validate() {
+				//手机号正则
+				var mPattern = /^1[34578]\d{9}$/;
+				if (!mPattern.test(this.phone)) {
+					uni.showToast({
+						title: '手机号格式不正确',
+						icon: 'none'
+					});
+					return false
+				}
+				// ...更多验证
+				return true
+			},
+			// 提交
+			submit() {
+				// 表单验证
+				if (!this.validate()) return;
+				// 提交后端
+				// 登录成功处理
 			}
 		}
 	}
